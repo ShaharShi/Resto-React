b89a31381760232badc0991bbd0e8fdfd87ed894
ShaharShi <Shaharshitrit01@gmail.com>
2020-09-12 14:36:31 +0300

    Initialize resto project

--- src/components/containers/pages/home/index.tsx
+++ src/components/containers/pages/home/index.tsx
@@ -1,11 +1,31 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import { MealsContext } from '../../../../App';
+import Axios from 'axios';
+import Meal from 'components/containers/ui-components/meal';
 // import { MealsContext } from 'App';
 
 export default function HomePage() {
     const [state, setState] = useContext(MealsContext)
+
+    async function getMealsFromServer() {
+        try {
+            const { data } = await Axios.get('http://localhost:5200/meals')
+            setState({...state, meals: [...state.meals, ...data]})
+        } catch (error) {
+            console.log(error)
+        } finally {
+            
+        }
+    }
+    
+    useEffect(()=> {
+        getMealsFromServer()
+    }, [])
+    
     
     return (
-    <div>Home</div>
+    <div className={"row mt-5 justify-content-between"}>{
+        state.meals.map((meal: any) => { return <Meal key={meal.name} actionTitle={'Add to Orders'} {...meal}/>})
+    }</div>
     )
 }
--- src/components/containers/ui-components/meal/index.tsx
+++ src/components/containers/ui-components/meal/index.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
-export default function Meal() {
+interface IMeal {
+    name: string;
+    description: string;
+    image: string;
+    action: Function;
+    actionTitle: string;
+    cls: string
+}
+
+export default function Meal(props: IMeal) {
     return (
-        <div></div>
+        <Card className="col-lg-4 mb-5">
+            <Card.Img variant="top" src={props.image} />
+            <Card.Body>
+                <Card.Title>{props.name}</Card.Title>
+                <Card.Text>
+                    {props.description}
+                </Card.Text>
+                <Button variant={props.cls || "primary"} >{props.actionTitle}</Button>
+            </Card.Body>
+        </Card>
     )
 }
